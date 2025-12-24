f0024dad24285460872d736229b54ac3010fa255
Angel Developer <amamouridis.dev@gmail.com>
2025-12-24 15:56:51 +0100

    i change a word in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       {/* Hero Section */}
       <section className="w-full h-screen bg-[url('/petra.jpg')] bg-cover bg-center flex items-center justify-center">
         <div className="bg-black/40 backdrop-blur-md p-6 rounded-xl shadow-xl">
-          <h1 className="text-4xl font-bold text-center">Discover Lesvos</h1>
+          <h1 className="text-4xl font-bold text-center">Lesvos</h1>
           <p className="text-center mt-2 text-lg">Beaches • Villages • Culture • Nature</p>
         </div>
       </section>
