ce09af3b7e5e20d6c6c36d525fd302a2c5f881ef
Angel Developer <amamouridis.dev@gmail.com>
2025-12-25 01:56:10 +0100

    changes the image for the village plomari

--- src/app/villages/plomari/page.tsx
+++ src/app/villages/plomari/page.tsx
@@ -14,7 +14,7 @@ export default function PlomariPage() {
       {/* HERO */}
       <section className="relative h-[60vh] w-full">
         <Image
-          src="/plomari1.jpg"
+          src="/plomari2.jpg"
           alt="Πλωμάρι Λέσβος"
           fill
           className="object-cover"
